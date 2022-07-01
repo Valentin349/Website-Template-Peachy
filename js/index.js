8dc11418cc8f13bd59ec1e177bb8e86e3b4a7b1b
valentin <valentino7007@gmail.com>
2022-07-01 23:06:06 +0100

    finished html and css

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var viewportHeight = document.documentElement.clientHeight;
+var pageHeight = document.documentElement.scrollHeight;
+var progress;
 
 const faders = document.querySelectorAll('.fade-in');
+const progressBar = document.querySelector('.progress-bar');
 
 const appearOptions = {
     threshold: 0.3,
@@ -17,6 +20,11 @@ const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
     })
 }, appearOptions);
 
+const updateProgressBar = (progress) => {
+    progressBar.ariaValueNow = progress;
+    progressBar.style = "width: " + progress + "%";
+};
+
 window.onscroll = () => {
     if (window.pageYOffset < viewportHeight/16){
         document.getElementById("title-banner").style.perspectiveOrigin = "50% calc(50% - 150px)";
@@ -26,6 +34,25 @@ window.onscroll = () => {
     }
     else if (window.pageYOffset > viewportHeight/8){
         document.getElementById("title-banner").style.perspectiveOrigin = "50% calc(50% - 30px)";
+    }
+    
+
+    if (window.pageYOffset > pageHeight*0.8){
+        updateProgressBar(100)
+    }
+    else if (window.pageYOffset > pageHeight*0.6){
+        updateProgressBar(80)
+    }
+    else if (window.pageYOffset > pageHeight*0.4){
+        updateProgressBar(60)
+    }
+    else if (window.pageYOffset > pageHeight*0.2){
+        updateProgressBar(40)
+    }
+    else if (window.pageYOffset > 0) {
+        updateProgressBar(20)
+    } else {
+        updateProgressBar(0)
     }
 
 };
